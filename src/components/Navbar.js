fc7a9086a7434f077ce34349bdc5c56fabc234ab
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-02-26 08:58:43 -0800

    Navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,26 +2,35 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between">
-      <div>
-        <Link to="/">
-          Orphan<span>Life</span>
-        </Link>
+    <div className="py-5 bg-orange-600">
+      <div className="container mx-auto flex justify-between items-center  text-white ">
+        <div className="text-xl">
+          <Link to="/">
+            Orphan
+            <span className="italic text-2xl font-semibold text-orange-900">
+              Life
+            </span>
+          </Link>
+        </div>
+
+        <ul className="flex gap-4 uppercase">
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        <div className="bg-white text-orange-600 py-2 px-6 rounded-sm hover:bg-orange-600 hover:text-white duration-300">
+          <a href="#" className="uppercase">
+            Donate Now
+          </a>
+        </div>
       </div>
-
-      <ul>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-
-      <a href="#">Donate Now</a>
     </div>
   );
 };
